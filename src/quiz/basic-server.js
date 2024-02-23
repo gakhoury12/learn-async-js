9cd97c6a2e00e46798aacac338632f301c131deb
Gabe Khoury <gabrielkhoury3@gmail.com>
2024-02-23 16:49:19 -0500

    classwork

--- src/quiz/basic-server.js
+++ src/quiz/basic-server.js
@@ -45,6 +45,10 @@ const server = http.createServer((req, res) => {
             res.end(JSON.stringify(incidents.filter((incident) => {
                 return incident.id === `MABOS00${id}`
             })));
+        }  else {
+        // If the request is for an unsupported endpoint, send a not found response
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Endpoint not found');
         }
     } else {
         // If the request is for an unsupported endpoint, send a not found response
